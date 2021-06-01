35cf5ad8e5f763138563fc530ec6808b4f538d74
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-06-01 20:43:37 +0200

    more structure changes

--- game.rb
+++ game.rb
@@ -7,8 +7,9 @@ end
 class BasePlayer
   attr_accessor :type
 
-  def initialize(type)
+  def initialize(type, humanity)
     @type = type
+    @humanity = humanity
   end
 
   def ==(other)
@@ -20,30 +21,47 @@ class BasePlayer
   end
   
   def guess
+
+  end
 end
 
 # Computer Player that plays algorithmically
 class CPUPlayer < BasePlayer
   def initialize(type)
-    @humanity = false
-    super(type)
+    super(type, false)
   end
-
 end
 
 # Human Player that plays via input
 class HumanPlayer < BasePlayer
   def initialize(type)
-    @humanity = true
-    super(type)
+    super(type, humanity)
   end
-  
 end
 
+# A board in which live the guess spaces and the mastermind code 
 class Board
-  def initialize
-    @board = gen_board
+  def initialize(spaces, code)
+    @board = Array.new(spaces) { BoardSpace.new }
+    @code = code
   end
 end
 
-class BoardLine
+# A space / line where a guess and a rating live
+class BoardSpace
+  def initialize
+    @space = { guess: Array.new(4), rating: Array.new(4) }
+  end
+
+  def guess=(guess)
+    return unless guess.count == 4
+
+    @space.guess = guess
+  end
+
+  def rating=(rating)
+    return unless rating.count == 4
+
+    @space.rating = rating
+  end
+end

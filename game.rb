aef517fbcf9e9dbca4805232a80adf81187adb53
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-06-01 20:27:52 +0200

    more refractoring to basic class structrue

--- game.rb
+++ game.rb
@@ -1,10 +1,12 @@
 module TypeHelper
-  TYPE_GUESS = "Codebreaker".freeze
-  TYPE_MAKER = "Codemaker".freeze
+  TYPE_GUESS = 'Codebreaker'.freeze
+  TYPE_MAKER = 'Codemaker'.freeze
 end
 
-class PlayerType
+# All players, human or computer, inherit common functionality from here
+class BasePlayer
   attr_accessor :type
+
   def initialize(type)
     @type = type
   end
@@ -12,19 +14,36 @@ class PlayerType
   def ==(other)
     @type == other.type
   end
+
+  def rate
+
+  end
+  
+  def guess
 end
 
-class BasePlayer
-  def initialize()
-
+# Computer Player that plays algorithmically
+class CPUPlayer < BasePlayer
+  def initialize(type)
+    @humanity = false
+    super(type)
   end
 
 end
 
-class CPUPlayer < BasePlayer
-
-end
-
+# Human Player that plays via input
 class HumanPlayer < BasePlayer
-
+  def initialize(type)
+    @humanity = true
+    super(type)
+  end
+  
 end
+
+class Board
+  def initialize
+    @board = gen_board
+  end
+end
+
+class BoardLine

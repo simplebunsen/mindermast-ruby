9fa5ee407294c99aaf2ae447caa6e2c78814484d
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-06-02 23:24:31 +0200

    new game object, output class

--- game.rb
+++ game.rb
@@ -5,3 +5,12 @@ require './helper'
 
 board = Board.new(5, "bepis")
 board.print
+puts "\e[0;35m this is a purple text \e[0m\n"
+
+class Game 
+  def initialize(gamemode)
+    @gamemode = gamemode
+
+  end
+
+end

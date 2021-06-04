932993804ae489d4502ffc700113d48f589cc6d3
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-06-04 23:43:12 +0200

    elaborated human player rate and guess code, new helper for string input to internal num system

--- helper.rb
+++ helper.rb
@@ -2,3 +2,9 @@ module TypeHelper
   TYPE_GUESS = 'Codebreaker'.freeze
   TYPE_MAKER = 'Codemaker'.freeze
 end
+
+module InputHelper
+  def self.color_to_internal(array)
+    # do magic to convert color array to internal
+  end
+end
--- players.rb
+++ players.rb
@@ -1,17 +1,13 @@
+require './helper'
 # All players, human or computer, inherit common functionality from here
 class BasePlayer
-  attr_accessor :type
 
-  def initialize(type, humanity)
-    @type = type
+  def initialize(humanity)
     @humanity = humanity
   end
 
-  def ==(other)
-    @type == other.type
-  end
-
   # implement rate, guess and make_code in the subclasses!!!
+
 end
 
 # Computer Player that plays algorithmically
@@ -41,5 +37,26 @@ end
 class HumanPlayer < BasePlayer
   def initialize
     super(true)
+  end
+
+  def rate
+    puts 'How many pins are both the right place and the right color?'
+    num_place_color = gets.chomp.to_i
+    puts 'How many pins are the right color BUT not the right place?'
+    num_color = gets.chomp.to_i
+    # TODO: map that to an array
+    rating = [2, 2, 1, 0]
+    return rating
+  end
+
+  def guess
+    puts 'guess like so: "red,blue,brown,green"'
+    color_array = gets.chomp.split(',')
+    InputHelper.color_to_internal(color_array)
+  end
+
+  def make_code
+    puts 'Set a code like so: "red,blue,brown,green"'
+    gets.chomp
   end
 end
